374c028e96a7b959b2103e239f62506750775b55
Avvi Singh <singh.avvi98@gmail.com>
2021-12-01 19:53:04 +1100

    display updating on number entering

--- js/index.js
+++ js/index.js
@@ -2,11 +2,13 @@ let runningNumber = "";
 let numberButton = document.getElementsByClassName(
     "calculator-body__buttons-number"
 );
+let displayText = document.getElementById("calculator-body__display-text");
 
 console.log(numberButton);
 
 let numberStringConcat = (n) => {
     runningNumber += n;
+    displayText.innerHTML = runningNumber;
     console.log(runningNumber);
     return runningNumber;
 };
