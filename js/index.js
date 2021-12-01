834e455ebab17e3993b9550812c68be6344e8800
Avvi Singh <singh.avvi98@gmail.com>
2021-12-01 21:38:45 +1100

    addition and equals button working

--- js/index.js
+++ js/index.js
@@ -1,14 +1,45 @@
 let runningNumber = "";
+let runningTotal = 0;
+let lastOperatorClicked = "";
 let numberButton = document.getElementsByClassName(
     "calculator-body__buttons-number"
 );
 let displayText = document.getElementById("calculator-body__display-text");
-
-console.log(numberButton);
+let equalsButton = document.getElementById("equalsButton");
+let plusButton = document.getElementById("plusButton");
 
 let numberStringConcat = (n) => {
     runningNumber += n;
     displayText.innerHTML = runningNumber;
-    console.log(runningNumber);
     return runningNumber;
 };
+
+let clearDisplayAndTotal = () => {
+    runningNumber = "";
+    runningTotal = 0;
+    lastOperatorClicked = "";
+    displayText.innerHTML = runningNumber;
+};
+
+let addToTotal = () => {
+    runningTotal += +runningNumber;
+    runningNumber = "";
+    lastOperatorClicked = "+";
+    displayText.innerHTML = runningTotal;
+};
+
+let subtractFromTotal = () => {};
+
+let divideTotal = () => {};
+
+let multiplyTotal = () => {};
+
+let displayTotal = () => {
+    if (lastOperatorClicked === "+") {
+        runningTotal += +runningNumber;
+    }
+    displayText.innerHTML = runningTotal;
+};
+
+plusButton.addEventListener("click", addToTotal);
+equalsButton.addEventListener("click", displayTotal);
